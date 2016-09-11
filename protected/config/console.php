279f8ab8c4e108c942a010fa49d86e91b926d882
mmohl <mmohammadl.lpkia@gmail.com>
2016-09-11 20:36:57 +0700

    inisialisasi

---
+++ protected/config/console.php
@@ -0,0 +1,73 @@
+<?php
+// This is the configuration for yiic console application.
+// Any writable CConsoleApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'My Console Application',
+
+	'modules'=>array(
+        #...
+        'user'=>array(
+            # encrypting method (php hash function)
+            'hash' => 'md5',
+
+            # send activation email
+            'sendActivationMail' => true,
+
+            # allow access for non-activated users
+            'loginNotActiv' => false,
+
+            # activate user on registration (only sendActivationMail = false)
+            'activeAfterRegister' => false,
+
+            # automatically login from registration
+            'autoLogin' => true,
+
+            # registration path
+            'registrationUrl' => array('/user/registration'),
+
+            # recovery password path
+            'recoveryUrl' => array('/user/recovery'),
+
+            # login form path
+            'loginUrl' => array('/user/login'),
+
+            # page after login
+            'returnUrl' => array('/user/profile'),
+
+            # page after logout
+            'returnLogoutUrl' => array('/user/login'),
+        ),
+		),
+
+	'commandMap'=>array(
+    'migrate'=>array(
+        'class'=>'system.cli.commands.MigrateCommand',
+        'migrationPath'=>'application.migrations',
+        'migrationTable'=>'tbl_migration',
+        'connectionID'=>'db',
+        'templateFile'=>'application.migrations.template',
+  	),
+	),
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// application components
+	'components'=>array(
+
+		// database settings are configured in database.php
+		'db'=>require(dirname(__FILE__).'/database.php'),
+
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+			),
+		),
+
+	),
+);
